7059b83adc22e8a1401d4403d82a5aea31f19639
Mazya <almazbekbekeev.@gmail.com>
2022-11-26 18:32:49 +0600

    Updates

---
+++ precache-manifest.7b6e2b8f07958f98e69a38c90d4c2460.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9469ed990231beda030d",
+    "url": "/static/css/main.5653408b.chunk.css"
+  },
+  {
+    "revision": "9469ed990231beda030d",
+    "url": "/static/js/main.9469ed99.chunk.js"
+  },
+  {
+    "revision": "2c02afda2375985545ff",
+    "url": "/static/js/1.2c02afda.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0a3dacdd721730658900f044c56ad093",
+    "url": "/index.html"
+  }
+];
