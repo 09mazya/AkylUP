7127b16144e2a4254c6e218a90b43bf7a0f75bb8
Aizhan <asanbaevazhan@gmail.com>
2022-11-26 14:01:47 +0600

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <div>Header
-        
+    <div>
+      <Link to="/" className='' > Главная </Link>
+      <Link to="/events" className='events' > Мероприятия </Link>
+      <Link to="/courses" className='courses' > Курсы </Link>
+      <Link to="/qa" className='q-a' > Вопросы и ответы  </Link>
     </div>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import Header from '../../components/Header/Header'
 
 function Home() {
   return (
     <div>
-      Home
-        ghfbds
+      <Header/>
     </div>
   )
 }
