d1eafcd2ed16cc3e1d6d46b3f8554a76a0eb313d
aktan <kerimbekov1233@gmail.com>
2022-11-26 14:01:55 +0600

    pull

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import { Route, Routes } from "react-router-dom"
-
+import Home from './pages/Home/Home'
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home/>} />
       </Routes>
     </div>
   );
