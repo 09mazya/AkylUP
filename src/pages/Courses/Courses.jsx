f099b73e1f8b8c3d0549c12d386a33cfa1499936
Erkin Kubatov <qoobatov@gmail.com>
2022-11-26 14:53:40 +0600

    create courses title

--- src/pages/Courses/Courses.jsx
+++ src/pages/Courses/Courses.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import "../Courses/Courses.css";
 
 function Courses() {
   return (
-    <div>Courses</div>
-  )
+    <>
+      <div className="courses-title-block">
+        <h1 className="courses-title">Навыки для правительств 21 века</h1>
+        <p className="courses-text">
+          Интерактивные онлайн-курсы для государственных служащих 21 века.
+          Освойте основные навыки и получите сертификат об окончании.
+        </p>
+      </div>
+      <div className="courses-list">
+          <div>
+            
+          </div>
+      </div>
+    </>
+  );
 }
 
-export default Courses
+export default Courses;
