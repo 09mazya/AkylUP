8671abcb6ad248ed3d6c809283ee5c5e1f849ad3
Undefind <aktan@MacBook-Air-Aktan.local>
2022-11-26 13:59:22 +0600

    router dom

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { Route, Routes } from "react-router-dom"
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
