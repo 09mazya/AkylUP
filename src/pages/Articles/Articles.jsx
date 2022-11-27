70af6509f487e1abe83d766a574142715576d82e
Erkin Kubatov <qoobatov@gmail.com>
2022-11-27 13:53:01 +0600

    fix pages

--- src/pages/Articles/Articles.jsx
+++ src/pages/Articles/Articles.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 import "../Articles/Articles.css";
 import DataArticles from "../../dataArticles.json";
-import Author from "../Articles/Articles-img/author.png"
+import Author from "../Articles/Articles-img/author.png";
 
 function Articles() {
   return (
     <>
       <div>
-        <h1>Статьи</h1>
-        <p className="article-text">Учитесь на опыте и знаниях своих коллег</p>
-        <button className="article-btn">Начать обучение</button>
+        <div className="article-head">
+          <div className="article-header-box">
+            <h1 className="article-title">Статьи</h1>
+            <p className="article-text">
+              Учитесь на опыте и знаниях своих коллег
+            </p>
+            <button className="article-btn">Начать обучение</button>
+          </div>
+        </div>
         {DataArticles.map((item) => (
           <div className="articles-list">
             <img className="articles-img" src={item.image} alt="" />
             <div>
               <p className="articles-list-p">{item.title}</p>
               <div className="autor-block">
-                <img src={Author} alt="" />
+                <img className="autor-block-img" src={Author} alt="" />
                 <p className="autor">Асанов Усон</p>
                 <p className="autor">26.11.2022</p>
               </div>
